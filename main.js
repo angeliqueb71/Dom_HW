040f78658d53a3b2c3cb12cc61d8370c9482d229
Angelique Barth <angeliqueb71@aol.com>
2017-03-20 12:07:58 -0400

    index.css

---
+++ main.js
@@ -0,0 +1,63 @@
+// console.log("test")
+//  Question 1
+// had help couldnt not get it
+
+// var button1 = document.getElementById("button1");
+// var button2 = document.getElementById("button2");
+//
+// button1.addEventListener("click",function() {
+//  text.innerText = "I am right";
+// })
+// button2.addEventListener("click",function() {
+//  text.innerText = "No, I am not right";
+// })
+
+
+// Question 2
+
+// document.getElementById("dh")
+// .addEventListener("mouseover", function(){
+// alert ("Hey! told you not to hover over me")
+// })
+
+
+
+// Question 3
+
+// document.getElementById("he")
+//  .addEventListener("click", function(){
+//  alert ("Hello")
+//  })
+
+
+
+// Question 4
+//had a hard time with this
+
+
+
+var fm = document.getElementById('fm')
+var eun = fm.getElementById('eun')
+var pwd = fm.getElementById('pwd')
+var add = document.getElementById('add')
+
+add.addEventListener("click", function(){
+
+// eun.value
+// pwd.value
+
+function add() {
+  // var eun = eun.value;
+  // var pwd = pwd.value;
+  console.log(eun.value, pwd.value)
+
+}
+
+
+if (eun.value) {
+    alert("Congrats on knowing your username and password!")
+} else {
+    alert("Incorrect username and password");
+}
+answer = "angeliquebarth"
+answer = "12345678"
